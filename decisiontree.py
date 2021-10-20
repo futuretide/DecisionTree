336a8418e7539f1dc5c3b0cd5d795d3ba121552e
SAMUELVJOSHUA <89632450+SAMUELVJOSHUA@users.noreply.github.com>
2021-10-20 16:43:34 +0530

    Updated Decision Tree Classifier

--- decisiontree.py
+++ decisiontree.py
@@ -26,3 +26,16 @@ graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
 graph.write_png('diabetes.png')
 Image(graph.create_png())
 print("Accuracy:",metrics.accuracy_score(Y_test, Y_pre))
+
+clf = DecisionTreeClassifier(criterion="entropy", max_depth=3)
+clf = clf.fit(X_train,Y_train)
+Y_pred = clf.predict(X_test)
+from IPython.display import Image
+from sklearn.tree import export_graphviz
+import pydotplus
+dot_data = StringIO()
+export_graphviz(clf, out_file=dot_data, filled=True, rounded=True,special_characters=True, feature_names = feature_columns,class_names=['0','1'])
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_png('diabetes.png')
+Image(graph.create_png())
+print("Accuracy:",metrics.accuracy_score(Y_test, Y_pred))
